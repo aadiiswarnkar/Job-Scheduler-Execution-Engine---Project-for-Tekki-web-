2cab5ee0df8ba4bffd5fb5d3af11f10241c56f72
Aditya Swarnkar <adityaswarnkar512@gmail.com>
2026-01-05 11:16:56 +0530

    Clean up session.py by removing unused code
    Removed commented-out SQLAlchemy setup code.

--- app/db/session.py
+++ app/db/session.py
@@ -47,32 +47,3 @@ def init_db():
 
 
 
-
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
-# from app.config import Config
-
-# engine = create_engine(
-#     Config.SQLALCHEMY_DATABASE_URI,
-#     pool_pre_ping=True
-# )
-
-# SessionLocal = sessionmaker(
-#     bind=engine,
-#     autoflush=False,
-#     autocommit=False
-# )
-
-# Base = declarative_base()
-
-
-# def init_db():
-#     # IMPORTANT: models import yahin hona chahiye
-#     from app.models.job import Job
-#     from app.models.job_execution import JobExecution
-
-#     Base.metadata.create_all(bind=engine)
-
-
-# print("🔌 DB URL USED BY WORKER:", Config.SQLALCHEMY_DATABASE_URI)
